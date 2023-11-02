11e71ed5dd0572b746647c0cb5bd734841955cab
Syaifun Nadhif <nadhifeldrafo@gmail.com>
2023-11-02 22:12:41 +0700

    ft: db post

--- src/config/key.js
+++ src/config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://nadhif:WbggjeF42ugMmKMM@cluster0.3sq8t9g.mongodb.net/?retryWrites=true&w=majority"
+    MONGOURI: "mongodb+srv://nadhif:WbggjeF42ugMmKMM@cluster0.3sq8t9g.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET_KEY: "lonlon"
 }
 // 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const {auth} = require('../controllers');
+const protected = require('../middlewares/requireLogin');
 
 
 router.get('/', auth.getHello);
-router.post('/register/', auth.register);
-router.post('/login/', auth.login);
+router.post('/register', auth.register);
+router.post('/login', auth.login);
+router.get('/whoami', protected.auth ,auth.whoami);
 
 module.exports = router;
