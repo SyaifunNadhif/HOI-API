6039e4eb250013e8a2d6a1d48a2356067379e507
SyaifunNadhif <nadhifeldrafo@gmail.com>
2024-01-01 15:57:24 +0700

    ft: compress foto

--- src/middlewares/requireLogin.js
+++ src/middlewares/requireLogin.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = process.env;
-const {User} = require('../db/models/'); 
+const { User } = require('../db/models/');
 
 module.exports = {
     protected: async (req, res, next) => {
@@ -10,7 +10,7 @@ module.exports = {
             if (!authorization) {
                 return res.status(401).json({
                     status: false,
-                    message: 'you\'re not authorized!',
+                    message: 'You\'re not authorized!',
                     data: null
                 });
             }
@@ -18,7 +18,7 @@ module.exports = {
             const data = await jwt.verify(authorization, JWT_SECRET_KEY);
 
             const user = await User.findById(data.id).select('-password');
-            const userInstance = new User(user); // Membuat instance model User
+            const userInstance = new User(user);
 
             if (!user) {
                 return res.status(401).json({
@@ -27,7 +27,16 @@ module.exports = {
                     data: null
                 });
             }
-            
+
+            // Menambahkan pengecekan user_type untuk akses 'basic' atau 'admin'
+            if (userInstance.user_type !== 'basic' && userInstance.user_type !== 'admin') {
+                return res.status(403).json({
+                    status: false,
+                    message: 'You don\'t have permission to access this resource!',
+                    data: null
+                });
+            }
+
             req.user = {
                 id: userInstance.id,
                 name: userInstance.name,
@@ -44,4 +53,3 @@ module.exports = {
         }
     }
 };
-
